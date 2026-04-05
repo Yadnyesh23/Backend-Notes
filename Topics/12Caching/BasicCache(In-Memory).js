ae15075ab058107876da3d1dc148c41df83b0010
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-04-05 11:34:07 +0530

    Basic Cache (In-Memory)

---
+++ Topics/12Caching/BasicCache(In-Memory).js
@@ -0,0 +1,52 @@
+import { promise } from 'bcrypt/promises'
+import express from 'express'
+
+const app = express()
+
+const cache = new Map()
+
+// Fake external api call
+const fethcWeatherData = async(city) => {
+    console.log(`Fecthing weather data...`)
+    return new Promise((resolve) => {
+        setTimeout((resolve) => {
+            resolve({
+                city,
+                temp : Math.floor(Math.random() * 30)
+            })
+        }, 1000);
+    })
+}
+
+// Api request to get weather
+app.get('/weather/:city', async(req, res) => {
+    const city = req.params
+    const key = `weather_${city}`
+
+    // Check if cache hit
+    if(cache.has(key)){
+        console.log(`Cache Hit...`)
+        // If hit, then return the response
+        return res.json({
+            source:"Cache",
+            data:cache.get(key)
+        })
+    }
+    // If cache miss
+    console.log(`Cache miss...`);
+    //  fetch the data from original api
+    const data = await fethcWeatherData(city)
+
+    // Store in cache
+    cache.set(key, {data, expiry: Date.now() + 10000}) // Updating the cache .It usually takes key , data and TTl(Time To Live) as a params
+    
+    // Send response in json
+    res.json({
+        source : 'api',
+        data : data
+    })
+})
+
+app.listen(3000, () => {
+    console.log(`Server listening...`)
+})
